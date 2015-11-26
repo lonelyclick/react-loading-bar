626263e028f5fd9f7f48b9b3af7f97b5ee99e27b
lonelyclick <lonelyclick@163.com>
2015-11-26 18:51:11 +0800

    fix eslint

--- mock.js
+++ mock.js
@@ -6,20 +6,21 @@ const route = require('koa-route')
 const fs = require('fs')
 const staticCache = require('koa-static-cache')
 const mount = require('koa-mount')
-const Koa = require('koa')
-const app = new Koa()
+const app = koa()
 
 const backendPort = 3000
 let deploy = 'examples'
 
-app.use(async (ctx, next) => {
-  const start = new Date
-  await next()
-  const ms = new Date - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
+app.use(logger())
 
 app.use(mount('/', staticCache(__dirname)))
+
+app.use(route.get('*', function* () {
+  this.type = 'html'
+  this.body = yield function (done) {
+    fs.readFile(`${deploy}/build/index.html`, 'utf8', done)
+  }
+}))
 
 app.listen(backendPort, function () {
   /* eslint-disable no-console */
